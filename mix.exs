6770b58dc45cd0762b77925ac7c2817e5647a34f
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2023-06-08 15:19:50 +0200

    Bump deps
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Phoenix.GenSocketClient.Mixfile do
     [
       app: :phoenix_gen_socket_client,
       version: @version,
-      elixir: "~> 1.8",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
@@ -35,7 +35,7 @@ defmodule Phoenix.GenSocketClient.Mixfile do
     [
       {:websocket_client, "~> 1.5", optional: true},
       {:jason, "~> 1.1", optional: true},
-      {:phoenix, "~> 1.3", only: :test},
+      {:phoenix, "~> 1.7", only: :test},
       {:cowboy, "~> 1.0", only: :test},
       {:credo, "~> 0.8.10", only: [:dev, :test], runtime: false},
       {:dialyze, "~> 0.2.1", only: :dev},
@@ -43,7 +43,7 @@ defmodule Phoenix.GenSocketClient.Mixfile do
     ]
   end
 
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["test/support", "lib"]
   defp elixirc_paths(_), do: ["lib"]
 
   defp package do
